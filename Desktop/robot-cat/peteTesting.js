4254655a41345a42c18a3e67b71903ccc65c1fb5
Peter Huynh <peterhyh@yahoo.com>
2023-02-17 14:10:02 -0600

    Javascript uodate

---
+++ Desktop/robot-cat/peteTesting.js
@@ -0,0 +1,10 @@
+const addByTwo = (num) => {
+    const answer = num + 2;
+    console.log(`${num} plus 2 equals to: ${answer}`);
+    
+}
+
+
+//enter in any number for 'x' below to add it by 2 and get an answer.
+//after entering the number in for 'x', type in the console, 'node peteTesting.js' to run it.
+addByTwo(x)
